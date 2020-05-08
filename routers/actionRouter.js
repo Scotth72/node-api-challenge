f7f59448555facca43aa6816880ab613de49d45c
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-05-08 16:52:24 -0500

    Finish for MVP, sqlite3, giving me issues still

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 		});
 });
 
-router.get('/:id', validateActionId, (req, res) => {
+router.get('/:id', (req, res) => {
 	db
 		.get(req.params.id)
 		.then((actions) => {
@@ -29,7 +29,7 @@ router.post('/:id', (req, res) => {
 	const { description, notes } = req.body;
 	const { project_id } = req.params.id;
 	db
-		.insert(description, notes, project_id)
+		.insert({ description, notes, project_id })
 		.then((response) => {
 			res.status(201).json(response);
 		})
@@ -48,33 +48,5 @@ router.put('/:id', (req, res) => {
 			res.status(500).json({ message: 'An Error has occurred updating' });
 		});
 });
-
-//custom Middleware
-function validateActionId(req, res, next) {
-	db
-		.get(req.params.id)
-		.then((response) => {
-			if (response) {
-				next();
-			} else {
-				res.status(400).json({ message: 'An Error has occurred' });
-			}
-		})
-		.catch((err) => {
-			res.satus(400).json({ message: 'Error has occurred' });
-		});
-}
-
-function validateAction(req, res, next) {
-	if (req.params.id) {
-		if (req.body.description.length > 0 && req.body.description.length < 128 && req.body.notes.length > 0) {
-			next();
-		} else {
-			res.status(400).json({ message: 'Please enter information correctly' });
-		}
-	} else {
-		res.status(400).json({ message: 'Error has occurred' });
-	}
-}
 
 module.exports = router;
