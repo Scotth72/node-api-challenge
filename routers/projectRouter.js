9504adb6c8c6958211a1a20092ba953216b5c35a
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-05-08 13:01:42 -0500

    Had issues with Sqlite3, but I am moving forward now

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -25,7 +25,44 @@ router.get('/:id', (req, res) => {
 		});
 });
 
+router.post('/', validateProject, (req, res) => {
+	db
+		.insert(req.body)
+		.then((removed) => {
+			res.status(200).json(removed);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'An Error with the server has occurred' });
+		});
+});
+
+router.delete('/:id', validateProjectId, (req, res) => {
+	db
+		.remove(res.params.id)
+		.then((removed) => {
+			res.status(200).json(removed);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Unable to remove project' });
+		});
+});
+
 //custom middleware
+
+function validateProjectId(req, res, next) {
+	db
+		.get(res.params.id)
+		.then((response) => {
+			if (response) {
+				next();
+			} else {
+				res.status(404).json({ message: 'Project does not exist' });
+			}
+		})
+		.catch((err) => {
+			res.status(400).json({ message: 'Invalid Id, does not exist' });
+		});
+}
 
 function validateProject(req, res, next) {
 	if (req.body.name.length > 0 && req.body.description.length > 0) {
